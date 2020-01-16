f1988bb9e21b5907f2090d81268437c5b87e5501
mariambenmoussa <41687067+mariambenmoussa@users.noreply.github.com>
2020-01-16 16:20:27 +0100

    fix ecs-datadog.tf

--- ecs_datadog.tf
+++ ecs_datadog.tf
@@ -12,7 +12,7 @@ resource "aws_ecs_task_definition" "datadog" {
     "environment": [{
       "name" : "DD_API_KEY",
       "value" : "${var.datadog-api-key}"
-    }
+    },
     {
       "name" : "DD_APM_ENABLED",
       "value" : "${var.datadog-apm}"
