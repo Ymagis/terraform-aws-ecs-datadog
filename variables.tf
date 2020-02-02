6ceabd0e05b672215debd002cb7b874349f84181
mariambenmoussa <benmoussa.mariam@gmail.com>
2020-02-02 12:58:45 +0100

    enable apm logs

--- variables.tf
+++ variables.tf
@@ -2,5 +2,9 @@ variable "env" {}
 variable "identifier" {}
 variable "datadog-api-key" {}
 variable "datadog-apm" {}
+variable "dd-logs-config-containers" {}
+variable "dd-logs-enable" {}
+variable "dd-ac-exlude" {}
 variable "datadog-extra-config" {}
 variable "ecs-cluster-id" {}
+
